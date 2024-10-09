cc2168a1c6ab8cc6a5005ef375204a37338b10b3
=Mic Gadman <mic.gadman@cgi.com>
2024-10-09 11:33:05 +0200

    added number validation

--- models/person.js
+++ models/person.js
@@ -19,7 +19,15 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function(n) {
+        return /^\d{2,3}-\d+/.test(n);
+      }
+    }
+  }
 });
 
 personSchema.set("toJSON", {
